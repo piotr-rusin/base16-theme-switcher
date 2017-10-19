70f68c16c85f1c61117df01429254cf31f264b84
Piotr Rusin <piotr.rusin88@gmail.com>
2017-10-19 12:57:54 +0200

    Add ConfigMapping class

--- base16_theme_switcher/config_structures.py
+++ base16_theme_switcher/config_structures.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from collections.abc import Mapping, MutableMapping
 
 
 class SetupError(Exception):
@@ -20,3 +21,77 @@ class ConfiguredPathError(SetupError, OSError):
 
 class ConfiguredFileNotFoundError(SetupError, FileNotFoundError):
     """A file provided by a user wasn't found or couldn't be created."""
+
+
+class ConfigMapping(MutableMapping):
+    """A multidimensional mapping of configuration options.
+
+    Each key is either a name of an option and is mapped to the option's value,
+    or a name of a section of the configuration and is mapped to a mapping
+    representing the contents of the section.
+    """
+
+    def __init__(self, data, ancestors):
+        """Create a configuration mapping.
+
+        :param data: a mapping containing configuration data
+        :param ancestors: the list of identifiers used to locate the mapping.
+            They can include names of all the parent sections of the given
+            configuration mapping or a name of a source of configuration data.
+        """
+        self._data = data
+        self._ancestors = ancestors
+
+    def __getitem__(self, key):
+        """Get a value mapped to the given key.
+
+        :param key: a key used to locate the requested value. It can be
+            a name of a configuration option or of a section of configuration.
+        :returns: a value of a configuration option or a subset of
+            configuration options. If the subset is being returned, it
+            is wrapped in a new instance of the ConfigMapping class.
+        :raises ConfigKeyError: if there is no option value or a subsection
+            of the config associated with the given key.
+        """
+        try:
+            value = self._data[key]
+        except KeyError:
+            raise ConfigKeyError(
+                'A requested configuration option "{}" is missing in {}'
+                ''.format(key, ':'.join(self._ancestors))
+            )
+
+        if (isinstance(value, Mapping) and not
+                isinstance(value, self.__class__)):
+            value = ConfigMapping(value, self._ancestors + (key,))
+        return value
+
+    def __setitem__(self, key, value):
+        """Assign a config subcestion or option value to a given name.
+
+        :param key: a name of a config section or option
+        :param value: a value of a configuration option or contents of
+            a section to be assigned to the given key.
+        """
+        self._data[key] = value
+
+    def __delitem__(self, key):
+        """Remove a config option or a section with a given name.
+
+        :param key: a name of an option or section to be removed.
+        """
+        del self._data[key]
+
+    def __iter__(self):
+        """Iterate on the names of config options or subsections.
+
+        :returns: an iterator object
+        """
+        return iter(self._data)
+
+    def __len__(self):
+        """Get the number of option and section names in the config.
+
+        :returns: the total number of option and section names.
+        """
+        return len(self._data)
---
+++ test/test_config_structures.py
@@ -0,0 +1,117 @@
+# -*- coding: utf-8 -*-
+
+import unittest
+
+from parameterized import parameterized
+
+from base16_theme_switcher.config_structures import (
+    ConfigKeyError,
+    ConfigMapping,
+)
+
+
+def get_by_key_chain(mapping, keys):
+    """Get a value from nested mapping by a chain of keys.
+
+    :param mapping: the mapping from which the value is extracted.
+    :param keys: a sequence of keys used to locate the requested value.
+    :returns: a value resulting from getting a value from the mapping by
+        the first key and from getting values from nested mappings by
+        remaining keys (if there are more than one).
+    """
+    selection = mapping
+    for k in keys:
+        selection = selection[k]
+    return selection
+
+
+class ConfigMappingTest(unittest.TestCase):
+    """Tests for ConfigMapping class."""
+
+    def prepareData(self):
+        """Prepare underlying data for the instance of tested class."""
+        self.data = {
+            'first': 123,
+            'second': 'abc',
+            'third': {
+                'third.first': 777,
+                'third.second': 'pp'
+            }
+        }
+
+    def setUp(self):
+        """Prepare the instance of the class to be used in tests."""
+        self.prepareData()
+        self.tested = ConfigMapping(self.data, ('root', ))
+
+    @parameterized.expand([
+        ('option_value', 'first'),
+        ('section', 'third'),
+        ('option_value_in_section', 'third', 'third.first')
+    ])
+    def test_getitem_returns_expected(self, _, *keys):
+        """Test if the method returns an expected result.
+
+        :param keys: a chain of keys to be used to access expected and
+            actual results.
+        """
+        expected = get_by_key_chain(self.data, keys)
+        actual = get_by_key_chain(self.tested, keys)
+        self.assertEqual(expected, actual)
+
+    @parameterized.expand([
+        ('key', 'fifth'),
+        ('key_in_section', 'third', 'fifth')
+    ])
+    def test_getitem_raises_ConfigKeyError_for_missing(self, _, *keys):
+        """Test if the method raises the exception.
+
+        :param keys: a chain of keys to be used to attempt to access a
+            non-existing value.
+        """
+        with self.assertRaises(ConfigKeyError) as ex:
+            get_by_key_chain(self.tested, keys)
+            self.assertEqual(
+                str(ex),
+                'The expected configuration option "{}" is missing '
+                'in {}'.format(
+                    keys[-1], ':'.join(['root'] + list(keys[:-1]))
+                )
+            )
+
+    @parameterized.expand([
+        ('an_option_value', 'option', 1345),
+        ('a_subsection', 'section', {'option': 44})
+    ])
+    def test_setitem_adds(self, _, key, value):
+        """Test if the method adds expected data.
+
+        :param key: a key to be used to index a new value.
+        :param value: a value to be added to the mapping.
+        """
+        self.tested[key] = value
+        self.assertEqual(value, self.tested[key])
+
+    @parameterized.expand([
+        ('an_option_value', 'first'),
+        ('a_subsection', 'third')
+    ])
+    def test_delitem_deletes(self, _, key):
+        """Test if the method deletes data.
+
+        :param key: a key of a value to be deleted from the mapping.
+        """
+        del self.tested[key]
+        self.assertNotIn(key, self.tested)
+
+    def test_len(self):
+        """Test if the number of items in the mapping is returned."""
+        expected = len(self.data)
+        actual = len(self.tested)
+        self.assertEqual(expected, actual)
+
+    def test_iter(self):
+        """Test the content and ordering of items in the iterator."""
+        expected = list(iter(self.data))
+        actual = list(iter(self.tested))
+        self.assertEqual(expected, actual)
