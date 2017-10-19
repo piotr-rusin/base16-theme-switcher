1583cb040d2027eb9d6c65f33f5d507c5ca9c121
Piotr Rusin <piotr.rusin88@gmail.com>
2017-10-19 11:55:43 +0200

    Add SetupError

--- base16_theme_switcher/config_structures.py
+++ base16_theme_switcher/config_structures.py
@@ -1,1 +1,10 @@
 # -*- coding: utf-8 -*-
+
+
+class SetupError(Exception):
+    """An error in application setup.
+
+    This exception and its subclasses are to be raised when an error in
+    configuration file or command line options was detected, or when
+    another exception may be a result of such an error.
+    """
