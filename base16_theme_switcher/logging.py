be056750e9a4f842b37b56b2abe3930f1c730ed7
Piotr Rusin <piotr.rusin88@gmail.com>
2017-10-24 19:33:36 +0200

    Add NotifySendHandler class

--- base16_theme_switcher/logging.py
+++ base16_theme_switcher/logging.py
@@ -1,1 +1,25 @@
 # -*- coding: utf-8 -*-
+import logging
+import subprocess
+
+
+class NotifySendHandler(logging.Handler):
+    """A handler showing messages as desktop notifications."""
+
+    def emit(self, record):
+        """Show the message in the record.
+
+        The message is shown using notify-send command provided by
+        libnotify package.
+
+        :param record: a log record containing a message to be displayed.
+        """
+        urgency_level = (
+            'normal' if record.levelno <= logging.INFO else 'critical'
+        )
+
+        subprocess.call(
+            'notify-send -u "{}" "Base16 Theme Switcher" "{}"'
+            ''.format(urgency_level, record.getMessage()),
+            shell=True
+        )
