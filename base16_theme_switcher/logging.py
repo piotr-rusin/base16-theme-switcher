81948a20457d270506b20372dcf373e90068bf22
Piotr Rusin <piotr.rusin88@gmail.com>
2017-10-24 19:35:58 +0200

    Add configure_root_logger function

--- base16_theme_switcher/logging.py
+++ base16_theme_switcher/logging.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 import logging
+from logging import handlers
 import subprocess
 
 
@@ -23,3 +24,26 @@ class NotifySendHandler(logging.Handler):
             ''.format(urgency_level, record.getMessage()),
             shell=True
         )
+
+
+def configure_root_logger(log_path, verbose=False):
+    """Configure the root logger.
+
+    :param log_path: a path to a file to be used by RotatingFileHandler.
+    :param verbose: set the level of the console handler to
+        logging.DEBUG if True, otherwise set the level to logging.ERROR.
+    """
+    logger = logging.getLogger()
+    logger.setLevel(logging.DEBUG)
+
+    console_handler = logging.StreamHandler()
+    console_level = logging.DEBUG if verbose else logging.ERROR
+    console_handler.setLevel(console_level)
+
+    logger.addHandler(console_handler)
+    with log_path as path:
+        file_handler = handlers.RotatingFileHandler(
+            str(path), maxBytes=10*1024*1024, encoding='utf-8'
+        )
+        file_handler.setLevel(logging.DEBUG)
+        logger.addHandler(file_handler)
