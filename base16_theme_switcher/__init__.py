fedae5c61c2eb0c892f80d1c9044b3b93da6196b
Piotr Rusin <piotr.rusin88@gmail.com>
2017-11-09 10:15:10 +0100

    Add project package attributes

--- base16_theme_switcher/__init__.py
+++ base16_theme_switcher/__init__.py
@@ -11,3 +11,8 @@ The application may be extended with support for different ways
 of prompting a user for a name of a theme to be set or for configuring
 themes for other applications.
 """
+__title__ = 'base16-theme-switcher'
+__version__ = '1.0.0b1'
+__author__ = 'Piotr Rusin'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017 Piotr Rusin'
