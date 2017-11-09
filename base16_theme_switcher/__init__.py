b30b5de6c6683c2100a84303d0a4c55f27398950
Piotr Rusin <piotr.rusin88@gmail.com>
2017-11-09 09:41:06 +0100

    Add project package docstring

---
+++ base16_theme_switcher/__init__.py
@@ -0,0 +1,13 @@
+"""
+======================
+base16-theme-switcher
+======================
+
+Base16-theme-switcher is an extensible color theme configuration tool
+for applications using colors provided in X resource database
+(configured in ~/.Xresources file).
+
+The application may be extended with support for different ways
+of prompting a user for a name of a theme to be set or for configuring
+themes for other applications.
+"""
